bdfe3c394d24faa0a8a882bde176d330a6c1e5c1
Yevtech <fojobor@byupathway.edu>
2025-09-28 09:06:39 +0100

    Triggered redeployment to clear warning context

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src/",
   publicDir: "public",
+  base: '/wdd330/', // Add this line
   build: {
     outDir: "../dist",
     rollupOptions: {
